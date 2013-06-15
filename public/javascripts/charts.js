081361195e0c0fe9739fd76b46a6a1c876dfe5f5
Andy Murdoch <andy@andymurdoch.com>
2013-06-15 20:46:25 +0930

    Graphs started

---
+++ public/javascripts/charts.js
@@ -0,0 +1,101 @@
+window.onload = function () {
+    var data = [
+        {
+            screen_name: '@johnaldrinpola',
+            count: 37
+        }, {
+            screen_name: '@asdfghJobeeel',
+            count: 34
+        }, {
+            screen_name: '@fictionalrp',
+            count: 33
+        }, {
+            screen_name: '@JGDJAIN',
+            count: 30
+        }, {
+            screen_name: '@EraSoWhatEver',
+            count: 20
+        }, {
+            screen_name: '@poorvagairola',
+            count: 18
+        }, {
+            screen_name: '@renzcenon',
+            count: 16
+        }, {
+            screen_name: '@IamMarkyG',
+            count: 13
+        }, {
+            screen_name: '@ahmedkotb',
+            count: 12
+        }, {
+            screen_name: '@jcklnt0616',
+            count: 11
+        }
+    ];
+
+var margin = {
+    top: 20, 
+    right: 20, 
+    bottom: 30, 
+    left: 130
+};
+var width = 400 - margin.left - margin.right;
+var height = 280 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data, function(datum) { return datum.count; })])
+    .rangeRound([0, width]);
+
+var y = d3.scale.ordinal()
+    .rangeRoundBands([0, height], .1);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+
+var svg = d3.select('#top_tweeters_chart')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+x.domain([0, d3.max(data, function(d) { return d.count; })]);
+y.domain(data.map(function(d) { return d.screen_name; }));
+
+svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+
+svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+svg.selectAll('rect')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', 3)
+    .attr('width', function(d) { return x(d.count); })
+    .attr('y', function(d) { return y(d.screen_name); })
+    .attr('height', y.rangeBand());
+
+svg.selectAll('.notation')
+    .data(data)
+    .enter().append('text')
+    .text(function(d) { console.log('text ' + d.count); return d.count; })
+    .attr('class', 'notation')
+    .attr('x', function(d) { return x(d.count); })
+    .attr('dx', '-0.5em')
+    .attr('width', function(d) { return x(d.count); })
+    .attr('y', function(d) { return y(d.screen_name) + y.rangeBand()/2; })
+    .attr('dy', '0.4em')
+    .attr('text-anchor', 'middle')
+    .attr('height', y.rangeBand());
+};
+
